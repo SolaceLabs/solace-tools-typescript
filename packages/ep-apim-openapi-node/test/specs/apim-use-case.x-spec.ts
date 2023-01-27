bc6a8c0e8cc9cd453c63c7384a27812d806b37aa
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2023-01-27 06:17:35 +0100

    apim mock server tests

--- packages/ep-apim-openapi-node/test/specs/apim-use-case.x-spec.ts
+++ packages/ep-apim-openapi-node/test/specs/apim-use-case.x-spec.ts
@@ -1,6 +1,8 @@
 import 'mocha';
 import { expect } from 'chai';
 import path from 'path';
+import builder from "@rsql/builder";
+import { emit } from "@rsql/emitter";
 import {
   TestContext,
 } from '@internal/tools/src';
@@ -16,6 +18,9 @@ import {
   EventApiProductsResponse, 
   EventApiProductsService,
   EventApiProductState,
+  GatewaysService,
+  MessagingService,
+  MessagingServiceResponse,
 } from '../../generated-src';
 import { TestHelpers } from '../lib/TestHelpers';
 
@@ -24,10 +29,21 @@ TestLogger.logMessage(scriptName, ">>> starting ...");
 
 const PublishDestiationsAttributeName = 'PUBLISH_DESTINATIONS';
 const PublishDestinationValue = 'TEST_APIM_API';
-const ApplicationDomainName = 'TEST_APIM_API';
-let ApplicationDomainId: string;
+// const ApplicationDomainName = 'TEST_APIM_API';
+// let ApplicationDomainId: string;
 const EventApiProductName = "EventApiProduct_1";
-const EventApiProductList: Array<EventApiProduct> = [];
+let EventApiProductId: string | undefined = undefined;
+let EventApiProductList: Array<EventApiProduct> = [];
+
+
+function encodeRFC3986URIComponent(str: string): string {
+  return encodeURIComponent(str)
+    .replace(
+      /[!'()*]/g,
+      (c) => `%${c.charCodeAt(0).toString(16).toUpperCase()}`
+    );
+}
+
 
 describe(`${scriptName}`, () => {
     
@@ -35,33 +51,7 @@ describe(`${scriptName}`, () => {
       TestContext.newItId();
     });
 
-    // Example query
-          // // https://github.com/piotr-oles/rsql#readme
-          // const ast = builder.and(
-          //   builder.in(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>('applicationDomainId'), [ApplicationDomainId]),
-          //   builder.eq(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>('brokerType'), EpSdkBrokerTypes.Solace),
-          //   builder.and(
-          //     builder.eq(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>("customAttributes.name"), PublishDestinationsAttribute.name),
-          //     builder.comparison(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>("customAttributes.value"), '=contains=', PublishDestinationsAttribute.value),                        
-          //     builder.eq(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>("customAttributes.name"), FlatImportAttribute.name),
-          //     builder.eq(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>("customAttributes.value"), FlatImportAttribute.value),                        
-          //     builder.or(
-          //       builder.and(
-          //         builder.eq(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>("customAttributes.name"), XOwningDomainIdAttribute.name),
-          //         builder.comparison(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>("customAttributes.value"), '=contains=', XOwningDomainIdAttribute.value),                        
-          //       ),
-          //       builder.and(
-          //         builder.eq(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>("customAttributes.name"), XSharingDomainIdAttribute.name),
-          //         builder.comparison(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>("customAttributes.value"), '=contains=', XSharingDomainIdAttribute.value),                        
-          //       ),
-          //     )
-          //   ),
-          //   builder.in(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>('version.state'), ["RELEASED", "DEPRECATED", "RETIRED", "DELETED"])
-          // );
-  
-          // const query = emit(ast);
-
-    it(`${scriptName}: should list all event api products and check attributes`, async () => {
+    it(`${scriptName}: should list all event api products and check attributes manually`, async () => {
       const PageSize = 100;
       try {
         const eventApiProductList: Array<EventApiProduct> = [];
@@ -94,23 +84,64 @@ describe(`${scriptName}`, () => {
           if(found.value !== PublishDestinationValue) return false;
           return true;
         });
-        // DEBUG
-        expect(false, TestLogger.createLogMessage('filteredList.length', filteredList.length)).to.be.true;
-        expect(false, TestLogger.createLogMessage('filteredList', filteredList)).to.be.true;
+        expect(filteredList.length, TestLogger.createLogMessage('filteredList.length', filteredList.length)).to.be.greaterThanOrEqual(1);
+        const found = filteredList.find( (eventApiProduct: EventApiProduct) => {
+          return eventApiProduct.name === EventApiProductName;
+        });
+        expect(found, TestLogger.createLogMessage(`filteredList.find(${EventApiProductName})`, filteredList)).to.not.be.undefined;
+        // // DEBUG
+        // expect(false, TestLogger.createLogMessage('filteredList', filteredList)).to.be.true;
+        EventApiProductList = filteredList;
+        EventApiProductId = found?.id;
       } catch(e) {
         expect(e instanceof ApiError, TestLogger.createNotApiErrorMessage(e.message)).to.be.true;
         expect(false, TestLogger.createApiTestFailMessage('failed', e)).to.be.true;
       }
     });
 
-    it(`${scriptName}: should list event api products with correct publish_destinations attribute`, async () => {
+    // TODO: server returns internal server error, fix server
+    xit(`${scriptName}: should list event api products with correct publish_destinations attribute using the query language`, async () => {
       try {
+
+        // // https://github.com/piotr-oles/rsql#readme
+        // const ast = builder.and(
+        //   builder.in(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>('applicationDomainId'), [ApplicationDomainId]),
+        //   builder.eq(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>('brokerType'), EpSdkBrokerTypes.Solace),
+        //   builder.and(
+        //     builder.eq(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>("customAttributes.name"), PublishDestinationsAttribute.name),
+        //     builder.comparison(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>("customAttributes.value"), '=contains=', PublishDestinationsAttribute.value),                        
+        //     builder.eq(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>("customAttributes.name"), FlatImportAttribute.name),
+        //     builder.eq(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>("customAttributes.value"), FlatImportAttribute.value),                        
+        //     builder.or(
+        //       builder.and(
+        //         builder.eq(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>("customAttributes.name"), XOwningDomainIdAttribute.name),
+        //         builder.comparison(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>("customAttributes.value"), '=contains=', XOwningDomainIdAttribute.value),                        
+        //       ),
+        //       builder.and(
+        //         builder.eq(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>("customAttributes.name"), XSharingDomainIdAttribute.name),
+        //         builder.comparison(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>("customAttributes.value"), '=contains=', XSharingDomainIdAttribute.value),                        
+        //       ),
+        //     )
+        //   ),
+        //   builder.in(EpSdkUtils.nameOf<EpSdkApimEventApiProductQueryFields>('version.state'), ["RELEASED", "DEPRECATED", "RETIRED", "DELETED"])
+        // );
+
+        // TODO: add your own operator => into an EpSdkApimQueryBuilder
+
+        // const query = emit(ast);
+
+        const attributeQueryAst = builder.comparison(`attributes.name[${PublishDestiationsAttributeName}].value`, '=elem=', `${PublishDestinationValue}.*`);
+        const attributeQuery = encodeRFC3986URIComponent(emit(attributeQueryAst));
+        // const attributeQuery = `attributes.name[${PublishDestiationsAttributeName}].value=elem=${PublishDestinationValue}.*`;
+
+
+        
         const eventApiProductList: Array<EventApiProduct> = [];
         let nextPage: number | null = 1;
         while(nextPage !== null) {
           const eventApiProductsResponse: EventApiProductsResponse = await EventApiProductsService.listEventApiProducts({
             pageNumber: nextPage,
-            query: `attributes.name[${PublishDestiationsAttributeName}].value:==${PublishDestinationValue}`  
+            query: attributeQuery
           });
           expect(eventApiProductsResponse.data, TestLogger.createApiTestFailMessage('failed')).to.not.be.undefined;
           // expect(eventApiProductsResponse.data.length, TestLogger.createApiTestFailMessage('failed')).to.be.lessThanOrEqual(PageSize);
@@ -129,60 +160,28 @@ describe(`${scriptName}`, () => {
       }
     });
 
-    // it(`${scriptName}: should get each event api product`, async () => {
-    //   try {
-    //     const eventApiProductList: Array<EventApiProduct> = await TestHelpers.getAllEventApiProducts();
-    //     for(const listedEventApiProduct of eventApiProductList) {
-    //       const eventApiProductResponse: EventApiProductResponse = await EventApiProductsService.getEventApiProduct({
-    //         eventApiProductId: listedEventApiProduct.id
-    //       });
-    //       expect(eventApiProductResponse.data, 'not equal').to.deep.equal(listedEventApiProduct);
-    //     }
-    //     EventApiProductList.push(...eventApiProductList);
-    //   } catch(e) {
-    //     expect(e instanceof ApiError, TestLogger.createNotApiErrorMessage(e.message)).to.be.true;
-    //     expect(false, TestLogger.createApiTestFailMessage('failed', e)).to.be.true;
-    //   }
-    // });
+    it(`${scriptName}: should get the gateways for all messaging services`, async () => {
+      expect(EventApiProductId, TestLogger.createLogMessage('EventApiProductId', EventApiProductId)).to.not.be.undefined;
+      try {
+        if(EventApiProductId === undefined) throw new Error('EventApiProductId === undefined');
+        const eventApiProductResponse: EventApiProductResponse = await EventApiProductsService.getEventApiProduct({ 
+          eventApiProductId: EventApiProductId,
+        });
+        const eventApiProduct: EventApiProduct = eventApiProductResponse.data;
+        expect(eventApiProduct.solaceMessagingServices.length, TestLogger.createLogMessage('eventApiProduct.solaceMessagingServices.length', eventApiProduct.solaceMessagingServices.length)).to.be.greaterThanOrEqual(1);
+        for(const solaceMessagingService of eventApiProduct.solaceMessagingServices) {
+          // get the gateway
+          const messagingServiceResponse: MessagingServiceResponse = await GatewaysService.getGateway({ gatewayId: solaceMessagingService.messagingServiceId });
+          const messagingService: MessagingService = messagingServiceResponse.data;
+          expect(messagingService.messagingServiceType, TestLogger.createLogMessage('messagingService.messagingServiceType', messagingService.messagingServiceType)).to.equal('solacexx');
 
-    // it(`${scriptName}: should query for event api products`, async () => {
-    //   try {
-    //     for(const listedEventApiProduct of EventApiProductList) {
-    //       // construct the query
-    //       expect(false, 'implement me').to.be.true;
-    //     }
-    //   } catch(e) {
-    //     expect(e instanceof ApiError, TestLogger.createNotApiErrorMessage(e.message)).to.be.true;
-    //     expect(false, TestLogger.createApiTestFailMessage('failed', e)).to.be.true;
-    //   }
-    // });
+        }
 
-    
-
-    // it(`${scriptName}: should get versions for each event api product and each single version`, async () => {
-    //   try {
-    //     for(const listedEventApiProduct of EventApiProductList) {
-    //       const eventApiProductVersionsResponse: EventApiProductVersionsResponse = await EventApiProductsService.getEventApiProductRevisions({
-    //         eventApiProductId: listedEventApiProduct.id
-    //       });
-    //       const eventApiProductVersionSummaryList: Array<EventApiProductVersionSummary>  = eventApiProductVersionsResponse.data;          
-    //       // // DEBUG
-    //       // expect(false, TestLogger.createLogMessage('eventApiProductVersionSummaryList', eventApiProductVersionSummaryList)).to.be.true;
-    //       for(const eventApiProductVersionSummary of eventApiProductVersionSummaryList) {
-    //         const eventApiProductResponse: EventApiProductResponse = await EventApiProductsService.getEventApiProductRevision({
-    //           eventApiProductId: listedEventApiProduct.id,
-    //           version: eventApiProductVersionSummary.version
-    //         });
-    //         const eventApiProduct: EventApiProduct = eventApiProductResponse.data;
-    //         expect(eventApiProduct.version, 'version mismatch').to.equal(eventApiProductVersionSummary.version);
-    //         expect(TestHelpers.getEventApiProductStateValueList(), 'state failed').to.include(eventApiProduct.state);
-    //       }
-    //     }
-    //   } catch(e) {
-    //     expect(e instanceof ApiError, TestLogger.createNotApiErrorMessage(e.message)).to.be.true;
-    //     expect(false, TestLogger.createApiTestFailMessage('failed', e)).to.be.true;
-    //   }
-    // });
+      } catch(e) {
+        expect(e instanceof ApiError, TestLogger.createNotApiErrorMessage(e.message)).to.be.true;
+        expect(false, TestLogger.createApiTestFailMessage('failed', e)).to.be.true;
+      }
+    });
 
 });
 
