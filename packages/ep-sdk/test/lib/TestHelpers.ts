44a323dfc59fc0967ce864ac5f0577d8c87db63d
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2023-03-11 13:18:08 +0100

    tests

--- packages/ep-sdk/test/lib/TestHelpers.ts
+++ packages/ep-sdk/test/lib/TestHelpers.ts
@@ -23,37 +23,26 @@ import {
 import { TestLogger } from "./TestLogger";
 
 export class TestHelpers {
-  public static applicationDomainAbsent = async ({
-    applicationDomainName,
-  }: {
+  
+  public static applicationDomainAbsent = async ({ applicationDomainName }: {
     applicationDomainName: string;
   }): Promise<void> => {
     const funcName = "applicationDomainAbsent";
     const logName = `${TestHelpers.name}.${funcName}()`;
     try {
-      const applicationDomainsResponse: ApplicationDomainsResponse =
-        await ApplicationDomainsService.getApplicationDomains({
-          name: applicationDomainName,
-        });
-      if (applicationDomainsResponse.data === undefined)
-        throw new Error(`${logName}: applicationDomainsResponse.data`);
-      if (applicationDomainsResponse.data.length > 1)
-        throw new Error(
-          `${logName}: applicationDomainsResponse.data.length > 1`
-        );
+      const applicationDomainsResponse: ApplicationDomainsResponse = await ApplicationDomainsService.getApplicationDomains({
+        name: applicationDomainName,
+      });
+      if (applicationDomainsResponse.data === undefined) throw new Error(`${logName}: applicationDomainsResponse.data`);
+      if (applicationDomainsResponse.data.length > 1) throw new Error(`${logName}: applicationDomainsResponse.data.length > 1`);
       if (applicationDomainsResponse.data.length === 1) {
-        const xvoid: void =
-          await ApplicationDomainsService.deleteApplicationDomain({
-            id: applicationDomainsResponse.data[0].id,
-          });
+        const xvoid: void = await ApplicationDomainsService.deleteApplicationDomain({
+          id: applicationDomainsResponse.data[0].id,
+        });
       }
     } catch (e) {
-      expect(
-        e instanceof ApiError,
-        TestLogger.createNotApiErrorMessage(e.message)
-      ).to.be.true;
-      expect(false, TestLogger.createApiTestFailMessage("failed", e)).to.be
-        .true;
+      expect(e instanceof ApiError, TestLogger.createNotApiErrorMessage(e.message)).to.be.true;
+      expect(false, TestLogger.createApiTestFailMessage("failed", e)).to.be.true;
     }
   };
 
