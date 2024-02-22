f322b6a2ccf3c0d370260a2cf0045e1822775218
Tamimi <tamimi@solace.com>
2024-02-22 15:12:21 -0800

    added examples dir

--- examples/ep-sdk.ts
+++ examples/ep-sdk.ts
@@ -11,6 +11,8 @@ import {
 import { OpenAPI as EpOpenApi} from "@solace-labs/ep-openapi-node";
 import { OpenAPI as EpRtOpenApi} from "@solace-labs/ep-rt-openapi-node";
 
+// This Example creates and deletes an Application Domain using EP SDK
+
 const main = async (): Promise<void> => {
   const ApplicationDomainName = "foo";
 
@@ -21,6 +23,7 @@ const main = async (): Promise<void> => {
   );
   EpSdkLogger.initialize({ epSdkLoggerInstance: epSdkConsoleLogger });
 
+  // Get Solace Cloud Token
   const cloudToken: string | undefined = process.env["SOLACE_CLOUD_TOKEN"];
   if (!cloudToken) throw new Error(`SOLACE_CLOUD_TOKEN env var missing`);    
 
@@ -44,14 +47,16 @@ const main = async (): Promise<void> => {
     },
   });
 
+  console.log("--- Creating Application domain ---")
   // Execute Application Domain Task
   const epSdkApplicationDomainTask_ExecuteReturn_Present: IEpSdkApplicationDomainTask_ExecuteReturn = await epSdkApplicationDomainTaskPresent.execute('my-context-id');
 
+  // Get Application Domain ID
   const applicationDomainId =
     epSdkApplicationDomainTask_ExecuteReturn_Present.epObject.id;
-  console.log(
-    `application domain name: ${ApplicationDomainName}\napplicationDomainId: ${applicationDomainId}.`
-  );
+  console.log(`application domain name: ${ApplicationDomainName}\napplicationDomainId: ${applicationDomainId}`);
+
+  console.log("\n--- Deleting Application domain ---")
 
   // Define Application Domain Task to delete application domain
   const epSdkApplicationDomainTaskAbsent = new EpSdkApplicationDomainTask({
@@ -61,9 +66,7 @@ const main = async (): Promise<void> => {
   
   // Execute Application Domain Task
   const epSdkApplicationDomainTask_ExecuteReturn_Absent: IEpSdkApplicationDomainTask_ExecuteReturn = await epSdkApplicationDomainTaskAbsent.execute('my-context-id');
-  console.log(
-    `application domain name deleted: ${ApplicationDomainName}\napplicationDomainId: ${epSdkApplicationDomainTask_ExecuteReturn_Absent.epObject.id}.`
-  );
+  console.log(`application domain name deleted: ${ApplicationDomainName}\napplicationDomainId: ${epSdkApplicationDomainTask_ExecuteReturn_Absent.epObject.id}`);
   
   // Transactional Data for the executed task
   // console.log(`epSdkApplicationDomainTask_ExecuteReturn_Absent.epSdkTask_TransactionLogData = ${JSON.stringify(epSdkApplicationDomainTask_ExecuteReturn_Absent.epSdkTask_TransactionLogData, null, 2 )}.`);
